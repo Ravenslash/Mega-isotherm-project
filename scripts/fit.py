8a9a1bc600113940f9cb2f1a5b1936c9e851588e
Noah W <46802986+Ravenslash@users.noreply.github.com>
2019-08-05 14:45:39 -0400

    Update and rename fit.py to scripts/fit.py

--- scripts/fit.py
+++ scripts/fit.py
@@ -1,7 +1,7 @@
 import pyiast
 import pandas as pd
 
-
+# replace path with csv containing isotherm data
 df_isotherm = pd.read_csv("/users/noahwhelpley/Downloads/Book2.csv")
 
 isotherm = pyiast.ModelIsotherm(df_isotherm, loading_key="Q"  ,pressure_key = "pressure" , model = "Langmuir")
